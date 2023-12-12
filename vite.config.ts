061e05160f69f5512a7baf38b555d96e6c394e54
gabrielmelim <gabrielmelim2012@hotmail.com>
2023-12-12 19:13:22 -0300

    deploy testing_5

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Especifique o diretório de saída desejado
-  },
+  base: '/br2win/',
 });
