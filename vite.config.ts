7fe0ce4dcd727459d94de85a579c62a2054ca7f9
gabrielmelim <gabrielmelim2012@hotmail.com>
2023-12-13 18:44:24 -0300

    deploy testing_53

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { URL } from 'url';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: 'https://br2win.com.br',
   server: {
     fs: {
+      // Para servir index.html em rotas desconhecidas
       strict: false,
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: new URL('index.html', import.meta.url).pathname,
+        404: new URL('public/404.html', import.meta.url).pathname,
+      },
     },
   },
 });
