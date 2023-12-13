5cfab8cd3ae099d3b00c9212a63f664338219c48
gabrielmelim <gabrielmelim2012@hotmail.com>
2023-12-13 12:35:21 -0300

    deploy testing_34

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,11 @@ export default defineConfig({
   plugins: [react()],
   base: 'https://br2win.com.br',
   server: {
-    fs: {
-      // Para servir index.html em rotas desconhecidas
-      strict: false,
-    },
-    // Adicionar reescritas para servir 404.html em rotas desconhecidas
     proxy: {
       '/': {
-        target: 'https://br2win.com.br/404.html', // Caminho para 404.html
+        target: 'https://br2win.com.br/index.html',
         changeOrigin: true,
-        rewrite: (path) => path !== '/404.html' ? '/404.html' : path,
+        rewrite: (path) => (path === '/' || path.endsWith('.html')) ? path : '/index.html',
       },
     },
   },
