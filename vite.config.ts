cdc3c93bfa6fcace6ab0360c689e186ba95efe6d
gabrielmelim <gabrielmelim2012@hotmail.com>
2023-12-13 19:01:42 -0300

    deploy testing_55

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,14 +9,6 @@ export default defineConfig({
     fs: {
       // Para servir index.html em rotas desconhecidas
       strict: false,
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: new URL('index.html', import.meta.url).pathname,
-        404: new URL('public/404.html', import.meta.url).pathname,
-      },
     },
   },
 });
