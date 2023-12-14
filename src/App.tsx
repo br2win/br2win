09f088a7c0d17798f41c3c1061c883628b4af823
gabrielmelim <gabrielmelim2012@hotmail.com>
2023-12-14 20:21:44 -0300

    deploy testing_85

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import React from 'react';
-import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import UniqueBcoinPage from './components/pages/UniqueBcoinPage/UniqueBcoinPage';
 import Footer from './components/footer/Footer';
 import Bcoin from './components/pages/bcoin/Bcoin';
 import { Help } from './components/help/Help';
-
-function NotFound() {
-  return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>Oops! Página não encontrada. Volte para <a href="/">página inicial</a> ou tente outra rota.</p>
-    </div>
-  );
-}
+import Qrcode from './components/qrcore/Qrcode';
+import Coin from './components/Coin/Coin';
 
 function App() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/bcoins" element={<Bcoin />} />
-          <Route path="/bcoin/:id" element={<UniqueBcoinPage />} />
-          <Route path="/ajuda" element={<Help />} />
-          
-          <Route path="*" element={<Navigate to="/404" />} />
+          <Route path='/bcoins' element={<Bcoin />} />
+          <Route path='/bcoin/:id' element={<UniqueBcoinPage />} />
+          <Route path='/ajuda' element={<Help />} />
+          <Route path='*' element={<Navigate to='/404' />} />
         </Routes>
         <Footer />
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
