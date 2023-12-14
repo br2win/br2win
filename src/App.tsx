f1f7f6cefc56d21dd0fd4712c59d26de4b902723
gabrielmelim <gabrielmelim2012@hotmail.com>
2023-12-14 20:08:34 -0300

    deploy testing_82

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import UniqueBcoinPage from './components/pages/UniqueBcoinPage/UniqueBcoinPage';
 import Footer from './components/footer/Footer';
@@ -6,8 +6,21 @@ import Bcoin from './components/pages/bcoin/Bcoin';
 import { Help } from './components/help/Help';
 import Qrcode from './components/qrcore/Qrcode';
 import Coin from './components/Coin/Coin';
+import { useEffect } from 'react';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Verificar se a rota é inválida e redirecionar para a página 404
+    const hash = window.location.hash;
+    if (hash && !document.getElementById(hash.slice(1))) {
+        navigate('/404');
+    }
+}, [navigate]);
+
+
   return (
     <>
       <HashRouter>
