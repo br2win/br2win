12fe805150d4a9a93f15fee05678ea06659580e0
gabrielmelim <gabrielmelim2012@hotmail.com>
2023-12-14 16:18:26 -0300

    deploy testing_74

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar'
-import UniqueBcoinPage from './components/pages/UniqueBcoinPage/UniqueBcoinPage'
-import Footer from './components/footer/Footer'
-import Bcoin from './components/pages/bcoin/Bcoin'
-import { Help } from './components/help/Help'
-import Qrcode from './components/qrcore/Qrcode'
-import Coin from './components/Coin/Coin'
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import UniqueBcoinPage from './components/pages/UniqueBcoinPage/UniqueBcoinPage';
+import Footer from './components/footer/Footer';
+import Bcoin from './components/pages/bcoin/Bcoin';
+import { Help } from './components/help/Help';
+import Qrcode from './components/qrcore/Qrcode';
+import Coin from './components/Coin/Coin';
 
 function App() {
-
   return (
     <>
-     <BrowserRouter>
-      <Navbar />
+      <HashRouter>
+        <Navbar />
         <Routes>
-          <Route path='/bcoins' element={<Bcoin />}></Route>
-          <Route path='/bcoin/:id' element={<UniqueBcoinPage />}></Route>
-          <Route path='/ajuda' element={<Help />}></Route>
+          <Route path='/bcoins' element={<Bcoin />} />
+          <Route path='/bcoin/:id' element={<UniqueBcoinPage />} />
+          <Route path='/ajuda' element={<Help />} />
         </Routes>
-      <Footer />
-      </BrowserRouter>  
+        <Footer />
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
